37045f9dad1d8561f5cfd22ed7e5323be05306e8
Abhas Arya <abhasarya@Abhass-MacBook-Pro.local>
2014-12-01 04:17:12 -0500

    pushing to heroku

--- Gemfile
+++ Gemfile
@@ -3,9 +3,9 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.8'
 gem 'devise'
+gem 'pg', '0.15.1'
 # Use sqlite3 as the database for Active Record
 group :development, :test do
-	gem 'sqlite3'
 	gem 'rspec-rails', '2.13.1'
 end
 
@@ -57,7 +57,6 @@ group :doc do
 end
 
 group :production do
-	gem 'pg', '0.15.1'
 	gem 'rails_12factor', '0.0.2'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20141201061731) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "clients", force: true do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -28,8 +31,8 @@ ActiveRecord::Schema.define(version: 20141201061731) do
     t.datetime "updated_at"
   end
 
-  add_index "clients", ["email"], name: "index_clients_on_email", unique: true
-  add_index "clients", ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true
+  add_index "clients", ["email"], name: "index_clients_on_email", unique: true, using: :btree
+  add_index "clients", ["reset_password_token"], name: "index_clients_on_reset_password_token", unique: true, using: :btree
 
   create_table "consultations", force: true do |t|
     t.string   "name"
